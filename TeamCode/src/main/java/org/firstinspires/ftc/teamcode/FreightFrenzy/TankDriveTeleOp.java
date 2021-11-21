0089b7ced250cdec90e6506d32c17fd055432bae
JackPeng11 <jackpeng3545@gmail.com>
2021-11-20 17:29:45 -0800

    meeting 2021-11-20: -added slowmode -added basic working teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/TankDriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy/TankDriveTeleOp.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.FreightFrenzy;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,7 +11,8 @@ public class TankDriveTeleOp extends LinearOpMode {
    @Override
    public void runOpMode() {
       initializeTankDriveHardware(TankDriveTeleOp.this);
-
+      Button toggleSlowMode = new Button();
+      boolean slowMode = false;
       telemetry.addLine("ready");
       telemetry.update();
 
@@ -27,10 +28,26 @@ public class TankDriveTeleOp extends LinearOpMode {
          leftPower = Range.clip(drive + turn, -0.7, 0.7);
          rightPower = Range.clip(drive - turn, -0.7, 0.7);
 
-         FL.setPower(leftPower);
-         FR.setPower(rightPower);
-         BL.setPower(leftPower);
-         BR.setPower(rightPower);
+         //speed modes
+         //if sped is true it's fast, if it's false it's normal
+         if (toggleSlowMode.isPressed(gamepad1.a)) {
+            slowMode = !slowMode;
+         }
+
+         if (slowMode) {
+            FL.setPower(leftPower * 0.5);
+            FR.setPower(rightPower * 0.5);
+            BL.setPower(leftPower * 0.5);
+            BR.setPower(rightPower * 0.5);
+
+         } else {
+            FL.setPower(leftPower);
+            FR.setPower(rightPower);
+            BL.setPower(leftPower);
+            BR.setPower(rightPower);
+         }
+         telemetry.addLine("slowmode:" + slowMode);
+         telemetry.update();
       }
    }
 }
